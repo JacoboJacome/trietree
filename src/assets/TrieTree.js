60045af39000f47efe6e0952a4ecd05369009f10
JacoboJacome <jacobojacome2@gmail.com>
2022-05-18 20:44:53 -0500

    first commit

--- src/assets/TrieTree.js
+++ src/assets/TrieTree.js
@@ -1,3 +1,55 @@
 import { TrieTreeNode } from "./TrieTreeNode";
 
-export class TrieTree {}
+export class TrieTree {
+  constructor() {
+    this.root = new TrieTreeNode(null);
+  }
+
+  findNode(prefix) {
+    //*regresa [node, depth] node -> el último nodo para el string dado (prefijo)
+    //* y depth -> numero de caracteres del prefijo que coinciden con los nodos de la estructura.
+  }
+
+  traverse(node, prefix, visit) {
+    //*Recorre el árbol de forma recursiva.
+  }
+
+  isEmpty() {
+    //*regresa true si el árbol está vacio o false y si no está vacio.
+  }
+
+  contains(str) {
+    //*regresa true si el string se encuentra en el tree trie.
+    let current = this.root;
+    for (let character of str) {
+      if (current.children[character] === undefined) {
+        return false;
+      }
+      current = current.children[character];
+    }
+    return current.isEndOfWord;
+  }
+
+  insert(str) {
+    //*agrega un string en el tree trie.
+    let current = this.root;
+
+    for (let character of str) {
+      if (current.children[character] === undefined) {
+        current.children[character] = new TrieTreeNode(character);
+      }
+
+      current = current.children[character];
+    }
+
+    current.isEndOfWord = true;
+  }
+
+  complete(prefix) {
+    //* regresa un arreglo de strings con el prefijo dado.
+  }
+
+  allTreeStrings() {
+    //*regresa un arreglo con todos los strings almacenados en el tree trie.
+  }
+}
--- src/assets/TrieTreeNode.js
+++ src/assets/TrieTreeNode.js
@@ -1,1 +1,26 @@
-export class TrieTreeNode {}
+export class TrieTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.isEndOfWord = false;
+    this.children = {};
+  }
+
+  isTerminal() {
+    //*regresa true si un nodo es terminal, es decir si el nodo
+    //*representa el último caracter de un string que almacenamos en el árbol
+  }
+
+  numChildren() {
+    //*regresa el número de hijos del nodo actual.
+  }
+  hasChild(character) {
+    //*regresa true si alguno de los hijos del nodo contiene un caracter dado.
+  }
+  getChild(character) {
+    //*regresa el nodo hijo que contiene/almacena un caracter dado.
+  }
+  addChild(character, child_node) {
+    //*agrega un nuevo nodo hijo al nodo actual. Deberás de
+    //*comprobar si el nodo actual tiene un hijo con el caracter que deseamos agregar.
+  }
+}
